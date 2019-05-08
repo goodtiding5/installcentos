56de94a681a899b338ebc5b82d2da30118b8797c
Kenneth Zhao <kenneth.zhao@gmail.com>
2019-05-07 17:49:21 -0700

    install epel repo first as packages need it

--- install-openshift.sh
+++ install-openshift.sh
@@ -87,20 +87,20 @@ echo "******"
 # install updates
 yum update -y
 
+#install epel
+yum -y install epel-release
+
+# Disable the EPEL repository globally so that is not accidentally used during later steps of the installation
+sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+
 # install the following base packages
-yum install -y  wget git zile nano net-tools docker-1.13.1 \
+yum -y --enablerepo=epel install wget git zile nano net-tools docker-1.13.1 \
 				bind-utils iptables-services \
 				bridge-utils bash-completion \
 				kexec-tools sos psacct openssl-devel \
 				httpd-tools NetworkManager \
 				python-cryptography python2-pip python-devel  python-passlib \
 				java-1.8.0-openjdk-headless "@Development Tools"
-
-#install epel
-yum -y install epel-release
-
-# Disable the EPEL repository globally so that is not accidentally used during later steps of the installation
-sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
 
 systemctl | grep "NetworkManager.*running" 
 if [ $? -eq 1 ]; then
