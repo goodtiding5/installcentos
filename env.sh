de6799b60796047e8282f4dd842d9db9aeacb932
Kenneth Zhao <kenneth.zhao@gmail.com>
2019-05-07 23:13:35 -0700

    update sample environment setup

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 ## Environment variables to control the install process#!/bin/bash
 
 DOMAIN=192-168-80-89.nip.io
-USERNAME=osadmin
-PASSWORD=ospass
+USERNAME=admin
+PASSWORD=password
 IP=192.168.80.89
 DISK=/dev/sdb
 LETSENCRYPT=false
-SCRIPT_REPO="https://raw.githubusercontent.com/goodtiding5/installcentos/master"
+PVS=false
